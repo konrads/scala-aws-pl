7d6b548fd4b2421af788e8d387a92496a4ee5a84
Konrad Sosnowski <konrad.sosnowski@ubnt.com>
2016-01-25 09:13:14 +0000

    adding list with pagination

--- app/aws_pl/aws/S3.scala
+++ app/aws_pl/aws/S3.scala
@@ -4,11 +4,12 @@ import java.io.InputStream
 
 import com.amazonaws.regions.Region
 import com.amazonaws.services.s3.S3ClientOptions
-import com.amazonaws.services.s3.model.AmazonS3Exception
+import com.amazonaws.services.s3.model.{AmazonS3Exception, ListObjectsRequest}
 import com.github.dwhjames.awswrap.s3.AmazonS3ScalaClient
 import play.Logger
 import play.api.libs.json.{JsValue, Json}
 
+import scala.collection.JavaConverters._
 import scala.concurrent.{ExecutionContext, Future}
 
 class S3(awsRegion: Region, endpoint: Option[String])(implicit ec: ExecutionContext) {
@@ -29,6 +30,31 @@ class S3(awsRegion: Region, endpoint: Option[String])(implicit ec: ExecutionCont
 
   def getJson(bucket: String, key: String): Future[Option[JsValue]] =
     streamFromS3(bucket, key)(in => Json.parse(in))
+
+  def list[T](bucket: String, prefix: String, filterPred: (String)=>Boolean, transform: (String)=>T): Future[Stream[T]] = {
+    val res = list(bucket, prefix)
+    res.map { stream =>
+      for {k <- stream if filterPred(k)} yield transform(k)
+    }
+  }
+
+  def list(bucket: String, prefix: String): Future[Stream[String]] = {
+    // iterates pagination
+    def listAllKeys(req: ListObjectsRequest, keysSoFar: Stream[String]): Future[Stream[String]] = {
+      val contentF = client.listObjects(req)
+      contentF.flatMap { ol =>
+        val keys = ol.getObjectSummaries.asScala.map(_.getKey)
+        val keysSoFar2 = keysSoFar #::: keys.toStream
+        if (ol.isTruncated) {
+          listAllKeys(req.withMarker(req.getMarker), keysSoFar2)
+        } else {
+          Future.successful(keysSoFar2)
+        }
+      }
+    }
+    val firstReq = new ListObjectsRequest().withBucketName(bucket).withPrefix(prefix)
+    listAllKeys(firstReq, Stream[String]())
+  }
 
   private def streamFromS3[T](bucket: String, key: String)(f: InputStream => T)(implicit ec: ExecutionContext): Future[Option[T]] =
     client.getObject(bucket, key)
